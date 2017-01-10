8d88656e58f2b97e2d8aa74a5939548d5b9150a2
Diego Perdomo <diegoperdomo_94@hotmail.com>
2017-01-10 08:38:54 -0400

    "Add GET /todos"

--- server/models/user.js
+++ server/models/user.js
@@ -1,12 +1,12 @@
 var mongoose = require('mongoose');
 
 var User = mongoose.model('User', {
-    email: {
-      type: String,
-      required: true,
-      minlength: 1,
-      trim: true
-    }
+  email: {
+    type: String,
+    required: true,
+    minlength: 1,
+    trim: true
+  }
 });
 
 module.exports = {User};
--- server/server.js
+++ server/server.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var bodyParser = require('body-parser');
 var {mongoose} = require('./db/mongoose');
+var {ObjectID} = require('mongodb');
 var {Todo} = require('./models/todo');
 var {User} = require('./models/user');
 
@@ -20,6 +21,29 @@ app.post('/todos', (req, res) => {
   });
 });
 
+app.get('/todos', (req, res) => {
+  Todo.find().then((docs) => {
+    res.send({docs});
+  }, (err) => {
+    res.status(400).send(err);
+  });
+});
+
+app.get('/todos/:id', (req, res) => {
+  var id = req.params.id;
+  if (!ObjectID.isValid(id)) {
+    return res.status(404).send();
+  }
+  Todo.findById(id).then((todo) => {
+    if (!todo) {
+      res.status(404).send("ID not found");
+    } 
+    res.send({todo});
+  });
+});
+
 app.listen(port, () => {
   console.log(`App is ready at port ${port}`);
 });
+
+module.exports = {app};
